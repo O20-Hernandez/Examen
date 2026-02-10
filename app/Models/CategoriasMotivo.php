266799eb7820bab2c1b26f7295df713e8f766067
O20-Hernandez <ah6746896@gmail.com>
2026-02-10 15:42:36 -0600

    Mi examen

---
+++ app/Models/CategoriasMotivo.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CategoriasMotivo extends Model
+{
+    /** @use HasFactory<\Database\Factories\CategoriasMotivoFactory> */
+    use HasFactory;
+}
